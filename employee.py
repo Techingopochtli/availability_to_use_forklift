2bef7fa1e269cdd909c78736339697075a4ad0fe
Andres Alvarez Pineda <95944584+Techingopochtli@users.noreply.github.com>
2023-08-25 22:51:32 -0600

    first version
    first functional version

---
+++ employee.py
@@ -0,0 +1,57 @@
+
+class Employee:
+
+    def __init__(self, name, last_name, s_last_name, id_emp):    # initialize Employee with id, name  and last names
+        self.sbp = 120
+        self.dbp = 80
+        self.pulse = 70
+        self.sp02 = 98
+        self.name = name
+        self.last_name = last_name
+        self.s_last_name = s_last_name
+        self.id = id_emp
+        self.glasses = False
+        self.alcohol = False
+        self.meals = 8
+        self.equilibrium_t = True
+        self.equilibrium_4r = True
+        self.equilibrium_4l = True
+        self.is_apt = True
+        self.is_extern = 0
+
+    def set_sbp(self, bp):  # set systolic blood pressure
+        self.sbp = bp
+
+    def set_dbp(self, bp):  # set diastolic blood pressure
+        self.sbp = bp
+
+    def set_pulse(self, pulse):  # set pulse
+        self.pulse = pulse
+
+    def set_sp02(self, sp):   # set percentage of oxygen in blood
+        self.sp02 = sp
+
+    def set_equilibrium_t(self, t):
+        self.equilibrium_t = t
+
+    def set_equilibrium_4r(self, r):
+        self.equilibrium_4r = r
+
+    def set_equilibrium_4l(self, l):
+        self.equilibrium_4l = l
+
+    def set_is_apt(self, apt):
+        self.is_apt = apt
+
+    def set_meals(self, meal):
+        self.meals = meal
+
+    def set_glasses(self, glass):
+        self.glasses = glass
+
+    def set_alcohol(self, alcohol):
+        self.alcohol = alcohol
+
+    def set_is_extern(self, extern):
+        self.is_extern = extern
+
---
+++ views.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, redirect, url_for, render_template, jsonify, request
+import xlwings as xw
+
+
+views = Blueprint(__name__, "views")
+
+
+@views.route("/", methods=['GET', 'POST'])
+def home():
+
+    return render_template("home.html", boolean=True)
+
+
+@views.route("/submit", methods=['GET', 'POST'])
+def submit():
+    col = list(request.form.keys())
+    new_data_raw = list(request.form.values())
+    master_wb = xw.Book("employees.xlsx")
+    master_sheets = master_wb.sheets["Empleados"]
+    newrow = master_sheets.range("A1").end("down").row + 1
+    print(newrow)
+    master_sheets.range(newrow, 1).value = new_data_raw
+    master_wb.sheets[0].range('A2').expand('right').copy()
+    master_wb.sheets[0].range('A2').expand().paste(paste='formats')
+    master_wb.save()
+
+    return render_template("submitted.html", boolean=True)
